b57594f14c8857e047e517f31da961788eeacdc3
David McCloskey <davmcclo@gmail.com>
2022-08-25 17:35:52 -0500

    Adding convenience batch files.

---
+++ setup-ninja-msvc-release.bat
@@ -0,0 +1,1 @@
+meson setup --backend=ninja --buildtype=release -Dgallium-drivers=zink build\release
---
+++ setup-vs-msvc-release.bat
@@ -0,0 +1,1 @@
+meson setup --backend=vs2019 --buildtype=release -Dgallium-drivers=zink build\release_vs2019

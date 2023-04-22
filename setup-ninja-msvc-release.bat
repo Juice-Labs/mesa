7f3afabb942c7e05d4b6eb77b6a3de237060e742
David McCloskey <dave@juicelabs.co>
2023-04-22 14:42:37 -0500

    Build egl

--- setup-ninja-msvc-release.bat
+++ setup-ninja-msvc-release.bat
@@ -1,1 +1,1 @@
-meson setup --backend=ninja --buildtype=release -Dgallium-drivers=zink build\release
+meson setup --backend=ninja --buildtype=release -Dgallium-drivers=zink -Dgles2=enabled -Degl=enabled -Dshared-glapi=enabled build\release
--- setup-vs-msvc-debugoptimized.bat
+++ setup-vs-msvc-debugoptimized.bat
@@ -1,1 +1,1 @@
-meson setup --backend=vs2019 --buildtype=debugoptimized -Dgallium-drivers=zink build\debugoptimized_vs2019
+meson setup --backend=vs2022 --buildtype=debugoptimized -Dgallium-drivers=zink -Dgles2=enabled -Degl=enabled -Dshared-glapi=enabled build\debugoptimized_vs2022

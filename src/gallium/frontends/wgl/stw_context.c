0a11b460215e0bc2d6dac797093af0841af66a76
Ramon Steenson <114740458+ramon-juicelabs@users.noreply.github.com>
2025-05-09 23:20:46 +1200

    When the context is the same the old framebuffer might already be acquired, meaning it will be presented next without a flush. Do this here
    Fixes assert and eventual crash in zink_kopper_present_queue when the framebuffer is queued for presentation

--- src/gallium/frontends/wgl/stw_context.c
+++ src/gallium/frontends/wgl/stw_context.c
@@ -436,6 +436,9 @@ stw_make_current(struct stw_framebuffer *fb, struct stw_framebuffer *fbRead, str
          if (old_ctx->current_framebuffer == fb && old_ctx->current_read_framebuffer == fbRead) {
             /* Return if already current. */
             return TRUE;
+         } else {
+            stw_st_flush(old_ctx->st, old_ctx->current_framebuffer->stfb,
+                            ST_FLUSH_FRONT);
          }
       } else {
          if(false) {

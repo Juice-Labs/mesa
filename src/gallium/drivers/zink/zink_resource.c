3e1ff62f1b7aa9efffcb41a4c209e709fd9f7ccb
Dean Beeler <canadacow@gmail.com>
2025-07-22 07:37:14 -0700

    WIP

--- src/gallium/drivers/zink/zink_resource.c
+++ src/gallium/drivers/zink/zink_resource.c
@@ -1315,14 +1315,16 @@ add_resource_bind(struct zink_context *ctx, struct zink_resource *res, unsigned 
    
    // Handle CUDA export binding - force export capabilities
    bool force_export = (bind & ZINK_BIND_CUDA_EXPORT) != 0;
+
+   struct winsys_handle whandle = {0};
+   whandle.type = ZINK_EXTERNAL_MEMORY_HANDLE;
+   whandle.handle = 0;
+   whandle.modifier = DRM_FORMAT_MOD_INVALID;
+   whandle.offset = 0;
+   whandle.stride = 0;
+   whandle.format = res->base.b.format;
    
-   if ((bind & ZINK_BIND_DMABUF) && !res->modifiers_count && screen->info.have_EXT_image_drm_format_modifier) {
-      res->modifiers_count = 1;
-      res->modifiers = malloc(res->modifiers_count * sizeof(uint64_t));
-      res->modifiers[0] = DRM_FORMAT_MOD_LINEAR;
-   }
-   
-   struct zink_resource_object *new_obj = resource_object_create(screen, &res->base.b, NULL, &res->linear, res->modifiers, res->modifiers_count, NULL, NULL);
+   struct zink_resource_object *new_obj = resource_object_create(screen, &res->base.b, &whandle, &res->linear, res->modifiers, res->modifiers_count, NULL, NULL);
    if (!new_obj) {
       debug_printf("new backing resource alloc failed!");
       res->base.b.bind &= ~bind;

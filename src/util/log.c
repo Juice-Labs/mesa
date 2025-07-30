f41c933b1234bab945ac45308072a7fd681ea209
Dean Beeler <canadacow@gmail.com>
2025-07-30 15:00:52 -0700

    Really, really make sure we can log in mesa

--- src/util/log.c
+++ src/util/log.c
@@ -83,7 +83,7 @@ init_juice_logging(void)
 #if defined(_WIN32)
    // Try to get juice logging from RemoteGPUVlk.dll
    const char* juiceLib = "RemoteGPUVlk.dll";
-   HMODULE juicevlk = GetModuleHandleA(juiceLib);
+   HMODULE juicevlk = LoadLibraryA(juiceLib);
    if (juicevlk)
       juice_log_output = (PFN_juice_log)GetProcAddress(juicevlk, "__wine_dbg_output");
 

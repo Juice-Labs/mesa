1e6d86727ab73db2e55549aad95155e0b14c9749
Dean Beeler <canadacow@gmail.com>
2025-07-29 17:56:34 -0700

    Log to our client.

--- src/util/log.c
+++ src/util/log.c
@@ -42,6 +42,34 @@
 
 #if DETECT_OS_WINDOWS
 #include <windows.h>
+
+/* Wine/Juice logging support - similar to vkd3d and dxvk */
+typedef int (*PFN_wine_log)(const char *);
+static PFN_wine_log wine_log_output = NULL;
+static int wine_log_initialized = 0;
+
+static void
+init_wine_logging(void)
+{
+   if (wine_log_initialized)
+      return;
+
+   /* Try to get wine logging from ntdll.dll first */
+   HMODULE module = LoadLibraryA("ntdll.dll");
+   if (module)
+      wine_log_output = (PFN_wine_log)GetProcAddress(module, "__wine_dbg_output");
+
+   /* Try to get juice logging from RemoteGPUVlk.dll (takes precedence) */
+   const char* juiceLib = "RemoteGPUVlk.dll";
+   HMODULE juicevlk = GetModuleHandleA(juiceLib);
+   if (!juicevlk)
+      juicevlk = LoadLibraryA(juiceLib);
+
+   if (juicevlk)
+      wine_log_output = (PFN_wine_log)GetProcAddress(juicevlk, "__wine_dbg_output");
+
+   wine_log_initialized = 1;
+}
 #endif
 
 enum mesa_log_control {
@@ -50,6 +78,7 @@ enum mesa_log_control {
    MESA_LOG_CONTROL_SYSLOG = 1 << 2,
    MESA_LOG_CONTROL_ANDROID = 1 << 3,
    MESA_LOG_CONTROL_WINDBG = 1 << 4,
+   MESA_LOG_CONTROL_WINE = 1 << 5,
    MESA_LOG_CONTROL_LOGGER_MASK = 0xff,
 
    MESA_LOG_CONTROL_WAIT = 1 << 8,
@@ -68,6 +97,7 @@ static const struct debug_control mesa_log_control_options[] = {
    { "syslog", MESA_LOG_CONTROL_SYSLOG },
    { "android", MESA_LOG_CONTROL_ANDROID },
    { "windbg", MESA_LOG_CONTROL_WINDBG },
+   { "wine", MESA_LOG_CONTROL_WINE },
    /* flags */
    { "wait", MESA_LOG_CONTROL_WAIT },
    { NULL, 0 },
@@ -116,18 +146,25 @@ mesa_log_init_once(void)
    mesa_log_control = parse_debug_string(os_get_option("MESA_LOG"),
          mesa_log_control_options);
 
+#if DETECT_OS_WINDOWS
+   init_wine_logging();
+#endif
+
    if (!(mesa_log_control & MESA_LOG_CONTROL_LOGGER_MASK)) {
       /* pick the default loggers */
 #if DETECT_OS_ANDROID
       mesa_log_control |= MESA_LOG_CONTROL_ANDROID;
+#elif DETECT_OS_WINDOWS
+      if (wine_log_output) {
+         mesa_log_control |= MESA_LOG_CONTROL_WINE;
+      } else {
+         mesa_log_control |= MESA_LOG_CONTROL_FILE;
+         /* stderr from windows applications without console is not usually
+          * visible, so communicate with the debugger instead */
+         mesa_log_control |= MESA_LOG_CONTROL_WINDBG;
+      }
 #else
       mesa_log_control |= MESA_LOG_CONTROL_FILE;
-#endif
-
-#if DETECT_OS_WINDOWS
-      /* stderr from windows applications without console is not usually
-       * visible, so communicate with the debugger instead */
-      mesa_log_control |= MESA_LOG_CONTROL_WINDBG;
 #endif
    }
 
@@ -418,6 +455,28 @@ logger_windbg(enum mesa_log_level level,
       free(msg);
 }
 
+static void
+logger_wine(enum mesa_log_level level,
+            const char *tag,
+            const char *format,
+            va_list va)
+{
+   if (!wine_log_output)
+      return;
+
+   char local_msg[1024];
+   char *msg = logger_vasnprintf(local_msg, sizeof(local_msg),
+         LOGGER_VASNPRINTF_AFFIX_TAG |
+         LOGGER_VASNPRINTF_AFFIX_LEVEL |
+         LOGGER_VASNPRINTF_AFFIX_NEWLINE,
+         level, tag, format, va);
+
+   wine_log_output(msg);
+
+   if (msg != local_msg)
+      free(msg);
+}
+
 #endif /* DETECT_OS_WINDOWS */
 
 /* This is for use with debug functions that take a FILE, such as
@@ -460,6 +519,7 @@ mesa_log_v(enum mesa_log_level level, const char *tag, const char *format,
 #endif
 #if DETECT_OS_WINDOWS
       { MESA_LOG_CONTROL_WINDBG, logger_windbg },
+      { MESA_LOG_CONTROL_WINE, logger_wine },
 #endif
    };
 

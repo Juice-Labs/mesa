bec8057b93839323b82c99ee792716bca5695004
Ramon Steenson <114740458+ramon-juicelabs@users.noreply.github.com>
2025-08-06 00:39:24 +1200

    Add bool all(bool) support for NV_GPU_shader5

--- src/compiler/glsl/builtin_functions.cpp
+++ src/compiler/glsl/builtin_functions.cpp
@@ -1386,7 +1386,7 @@ private:
    BA1(equal);
    BA1(notEqual);
    B1(any);
-   B1(all);
+   BA1(all);
    B1(not);
    BA2(textureSize);
    BA1(textureSamples);
@@ -2846,9 +2846,10 @@ builtin_builder::create_builtins()
                 NULL);
 
    add_function("all",
-                _all(&glsl_type_builtin_bvec2),
-                _all(&glsl_type_builtin_bvec3),
-                _all(&glsl_type_builtin_bvec4),
+                _all(nv_gpu_shader5, &glsl_type_builtin_bool),
+                _all(always_available, &glsl_type_builtin_bvec2),
+                _all(always_available, &glsl_type_builtin_bvec3),
+                _all(always_available, &glsl_type_builtin_bvec4),
                 NULL);
 
    add_function("not",
@@ -7876,10 +7877,10 @@ builtin_builder::_any(const glsl_type *type)
 }
 
 ir_function_signature *
-builtin_builder::_all(const glsl_type *type)
+builtin_builder::_all(builtin_available_predicate avail, const glsl_type *type)
 {
    ir_variable *v = in_var(type, "v");
-   MAKE_SIG(&glsl_type_builtin_bool, always_available, 1, v);
+   MAKE_SIG(&glsl_type_builtin_bool, avail, 1, v);
 
    const unsigned vec_elem = v->type->vector_elements;
    body.emit(ret(expr(ir_binop_all_equal, v, imm(true, vec_elem))));

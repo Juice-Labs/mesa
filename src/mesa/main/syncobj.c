e6eba2e4821c642781d89d32f95e54ff5cbbdeef
Dean Beeler <canadacow@gmail.com>
2025-08-10 14:48:29 -0700

    change sync errors to warnings, still logging.
    relax mesa on the border parameter (adjusting and warning)

--- src/mesa/main/syncobj.c
+++ src/mesa/main/syncobj.c
@@ -279,8 +279,7 @@ delete_sync(struct gl_context *ctx, GLsync sync, bool no_error)
 
    syncObj = _mesa_get_and_ref_sync(ctx, sync, true);
    if (!no_error && !syncObj) {
-      _mesa_error(ctx, GL_INVALID_VALUE,
-                  "glDeleteSync (not a valid sync object)");
+      _mesa_warning(ctx, "glDeleteSync (not a valid sync object)");
       return;
    }
 
@@ -432,8 +431,7 @@ _mesa_ClientWaitSync(GLsync sync, GLbitfield flags, GLuint64 timeout)
 
    syncObj = _mesa_get_and_ref_sync(ctx, sync, true);
    if (!syncObj) {
-      _mesa_error(ctx, GL_INVALID_VALUE,
-                  "glClientWaitSync (not a valid sync object)");
+      _mesa_warning(ctx, "glClientWaitSync (not a valid sync object)");
       return GL_WAIT_FAILED;
    }
 
@@ -504,8 +502,7 @@ _mesa_WaitSync(GLsync sync, GLbitfield flags, GLuint64 timeout)
 
    syncObj = _mesa_get_and_ref_sync(ctx, sync, true);
    if (!syncObj) {
-      _mesa_error(ctx, GL_INVALID_VALUE,
-                  "glWaitSync (not a valid sync object)");
+      _mesa_warning(ctx, "glWaitSync (not a valid sync object)");
       return;
    }
 

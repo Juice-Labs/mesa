5c0e1df1741c9d3237d2d06d54f493e308a474f5
Dean Beeler <canadacow@gmail.com>
2025-08-13 21:09:23 -0700

    dump images after glBufferData

--- src/mesa/main/mtypes.h
+++ src/mesa/main/mtypes.h
@@ -3685,6 +3685,9 @@ struct gl_context
 
    struct pipe_draw_start_count_bias *tmp_draws;
    unsigned num_tmp_draws;
+
+   /** Flag to track uniform buffer data updates for draw call dumping */
+   bool _UniformBufferDataUpdated;
 };
 
 #ifndef NDEBUG

0091cd382f4fa53fdae64c16215f2267aae29030
Alexandre Quadra <alexandre.quadra@hp.com>
2026-08-05 23:54:10 -0300

    Preventing call to a null glMakeBufferNonResidentNV on OptiX Shutdown

--- src/mesa/main/consts_exts.h
+++ src/mesa/main/consts_exts.h
@@ -268,6 +268,7 @@ struct gl_extensions
    GLboolean NV_primitive_restart;
    GLboolean NV_shader_atomic_float;
    GLboolean NV_shader_atomic_int64;
+   GLboolean NV_shader_buffer_load;
    GLboolean NV_gpu_shader5;
    GLboolean NV_texture_barrier;
    GLboolean NV_texture_env_combine4;
